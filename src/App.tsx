ad0ef3d7e66c56ed667cd616280f24f33ab1c1d9
cortella <bruno.nicolau@dtidigital.com.br>
2021-07-07 01:16:52 -0300

    End First template application

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { GlobalStyle } from "./assets/styles/global";
+import { DashBoard } from "./components/Dashboard";
+
 import { Header } from "./components/Header";
+import { TransactionsTable } from "./components/TransactionsTable";
 
 
 export function App() {
   return (
     <>
       <Header />
+      <DashBoard />
+      <TransactionsTable />
       <GlobalStyle />
     </>
   );
